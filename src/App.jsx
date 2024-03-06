ca5049eeade40d3bdb193591a18775546eae3762
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-03-06 23:04:31 +0100

    Mise en place de la structure simple

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import Nav from "./Components/Scenes/Nav/Nav";
+import Render from "./Components/Scenes/Render/Render";
 import "./Css/main.css"
 
 function App() {
   return (
     
       <div className="app">
-       
+        <Nav/>
+        <Render/>
       </div>
   );
 }
