60492939e16aefab31cf8778c830dd7682fdbd1c
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-03-16 18:01:39 +0100

    Lock et Unlock pour le renommage des dossiers

--- src/Components/Scenes/Task/Header/Task.jsx
+++ src/Components/Scenes/Task/Header/Task.jsx
@@ -1,28 +1,50 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import useAddTask from "./useTask";
+import React from "react";
+import useHeaderTask from "./useTask";
 
 export default function Header_Task(){
 
     
 
-    const {addTask, deleteFolder, folderSelectedName, handleChange} = useAddTask()
+    const {
+        addTask, 
+        deleteFolder, 
+        folderSelectedName, 
+        handleChange,
+        folderInputRef,
+        folderInputDisabled,
+        lockUnlockFolder
+    } = useHeaderTask()
 
+    
+
+    
+
+    
+
+    
 
     
     return(
         <div className="addTask_Display">
+
+
+
+            <div className="folderIndicator_Box">
+                <i onClick={deleteFolder} className="deleteFolder fa-solid fa-trash"></i>
+                <span>/ </span>
+                <input ref={folderInputRef} style={!folderInputDisabled ? {outline:"1px solid white"} : null} disabled={folderInputDisabled} onChange={handleChange} type="text" value={folderSelectedName ? folderSelectedName : ""} />
+                {folderInputDisabled && (<i onClick={lockUnlockFolder} className="lockFolder fa-solid fa-lock"></i>)}
+                {!folderInputDisabled && (<i onClick={lockUnlockFolder} className="unlockFolder fa-solid fa-unlock"></i>)}
+            </div>
+
+
 
             <div onClick={addTask} className="addTask_Box">
                 <i className="fa-solid fa-plus"></i>
                 <span >Add Task</span>
             </div>
 
-            <div className="folderIndicator_Box">
-                <i onClick={deleteFolder} className="fa-solid fa-trash"></i>
-                <span>/ </span>
-                <input onChange={handleChange} type="text" value={folderSelectedName ? folderSelectedName : "Default"} />
-            </div>
+            
         </div>
     )
 }
--- src/Components/Scenes/Task/Header/useTask.jsx
+++ src/Components/Scenes/Task/Header/useTask.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { update_taskOnCreation } from "../TaskSlice";
 import useLocalStorage from "../../../../Utils/useLocalStorage";
+import { useEffect, useRef, useState } from "react";
 
 export default function useHeaderTask(){
 
@@ -9,11 +10,19 @@ export default function useHeaderTask(){
     const {localStorage_renameFolder, localStorage_deleteFolder} = useLocalStorage()
 
     const dispatch = useDispatch()
+    const folderInputRef = useRef()
 
+    // Responsable de si oui ou non, on laisse l'input folderName etre accessible
+    const [folderInputDisabled, setFolderInputDisabled] = useState(true)
+    // Responsable de la mémorisation du folderName avant modification
+    const [folderNameMemory, setFolderNameMemory] = useState(null)
+
+    // Signale qu'on souhaite ajouter une tâche
     const addTask = () => {
         dispatch(update_taskOnCreation(!taskOnCreation))
     }
 
+    // Signale qu'on souhaite supprimer ce dossier
     const deleteFolder = () => {
         const userValidDelete = window.confirm(`Are you sure, delete ${folderSelectedName} ?`)
         if(userValidDelete){
@@ -21,12 +30,71 @@ export default function useHeaderTask(){
         }
     }
 
+    // Quand la valeur dans l'input change
     const handleChange = (e) => { localStorage_renameFolder(e.target.value) }
+
+    // Permet de rendre l'inputFolderName Accessible ou non
+    const lockUnlockFolder = () => {
+        setFolderInputDisabled(!folderInputDisabled)
+        setFolderNameMemory(folderSelectedName)
+    }
+
+    // Controle pour verifier que le nom du dossier n'est pas vide
+    const controlFolderNameNotEmpty = (folderName) => {
+        return /\S/.test(folderName)
+    }
+
+    // Permet de mettre le focus sur l'input
+    const focusInputFolder = (folderRef) => {
+        folderRef.selectionStart = folderRef.value.length
+        folderRef.selectionEnd = folderRef.value.length
+        folderRef.focus()
+    }
+
+
+
+    // Gère les listener de validation de changement de nom de l'input folderName pour le repasser en lock
+    useEffect(() => {
+        if(!folderInputDisabled && folderInputRef.current){
+
+            focusInputFolder(folderInputRef.current) // On focus l'input
+
+            const handleClickOutside = (e) => {
+                if(e.target !== folderInputRef.current){
+                    if(!controlFolderNameNotEmpty(folderInputRef.current.value)){
+                        window.alert("The name of your folder cannot be empty.")
+                        localStorage_renameFolder(folderNameMemory)
+                    }else{
+                        setFolderInputDisabled(true)
+                    }
+                    
+                }
+            }
+            const handleKeyDown = (e) => {
+                if(e.key === "Enter"){
+                    if(!controlFolderNameNotEmpty(folderInputRef.current.value)){
+                        window.alert("The name of your folder cannot be empty.")
+                        localStorage_renameFolder(folderNameMemory)
+                    }else{
+                        setFolderInputDisabled(true)
+                    }
+                }
+            }
+
+            setTimeout(() => {window.addEventListener("click", handleClickOutside)}, 1);
+            folderInputRef.current.addEventListener("keydown", handleKeyDown)
+
+            return () => window.removeEventListener("click", handleClickOutside)
+        } 
+    }, [folderInputDisabled])
 
     return{
         addTask,
         deleteFolder,
         folderSelectedName,
-        handleChange
+        handleChange,
+        folderInputRef,
+        folderInputDisabled,
+        lockUnlockFolder
     }
 }
