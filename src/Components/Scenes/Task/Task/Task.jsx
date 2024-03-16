3d9a830c3780ff6a3b4f1a3f9da979b7c007db08
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-03-16 17:01:18 +0100

    Correction bug mineur

--- src/Components/Scenes/Task/Task/Task.jsx
+++ src/Components/Scenes/Task/Task/Task.jsx
@@ -14,8 +14,6 @@ export default function One_Task({task, folderIndex}){
         validTask
     } = useTask_One(folderIndex, task)
 
-    const [taskTitle, setTaskTitle] = useState(task.title)
-
     return(
         <div className="task_Display">
             <div className="check_Box">
@@ -24,7 +22,7 @@ export default function One_Task({task, folderIndex}){
 
             <div style={(taskEditable && taskOnEdition) ? {outline:"1px solid rgb(0, 182, 227)", boxShadow:"0px 0px 10px rgb(0, 182, 227)"} : null} ref={taskRef} className={task.finish === true ? "task_Box taskFinish" : "task_Box"}>
                 <div style={(taskEditable && taskOnEdition) ? {cursor:"text"} : null} className="leftSideTask">
-                    <span ref={taskNameRef} contentEditable={taskEditable} className="taskName">{taskTitle}</span>
+                    <span ref={taskNameRef} contentEditable={taskEditable} className="taskName">{task.title}</span>
                 </div>
                 <div className="rightSideTask">
                     {!taskEditable && (<i onClick={toggleRenameTask} className="fa-solid fa-pen"></i>)}
