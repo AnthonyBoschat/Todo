5db6ba604507050202476b4c21fe21a1dc932406
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-03-18 17:48:57 +0100

    Synchronisation git

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,27 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
+require("dotenv").config() // Permet l'utilisation des variables d'environnement (.env)
+// Importation des module nécessaire
+const express = require("express") // Créé serveur http
+const mongoose = require("mongoose") // Intéragi avec mongoDB
+const cors = require("cors") // Active la politique de partage de ressource entre origine
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express() // app => nouvelle application express pour configurer le serveur
+const PORT = process.env.PORT || 5000 // Port sur lequel le serveur va écouter (Inutile pour le moment ?)
 
-const databaseURL = process.env.Mongo_URL
+const databaseURL = process.env.Mongo_URL // url de la base de donnée
 
+
+
+// Connection à mongodb
 mongoose.connect(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connexion réussie à MongoDB'))
-  .catch(err => console.error('Erreur de connexion à MongoDB', err));
+  .then(() => console.log('Connexion réussie à MongoDB')) // Quand la connection est faite, affiche => "connexion réussi"
+  .catch(err => console.error('Erreur de connexion à MongoDB =>', err)); // En cas d'erreur, affiche => "Erreur de connexion à mongoDB => err"
+
+// Mise en place de l'écoute sur le port : 3000
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port : ${PORT}`)
+})
+
+// Définir une route de test simple :
+app.get("/", (req,res) => {
+  res.send("Hello World !")
+})
