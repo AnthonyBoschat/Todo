5a4d633e3eb2b858e408bc5a8697f9f053b01327
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-04-23 16:55:29 +0200

    Mise en place des listes

---
+++ Frontend/src/Components/List/Detail/List_Detail.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function List_Detail({list, listState}){
+
+    const  {listVisible} = listState
+
+    return(
+        <div className={`listDetail_Display ${listVisible ? "visible" : "hidden"}`}>
+            {listVisible && (
+                <div className="listDetail_Box">
+                    <div className="listDetail_Item">
+                        {list.name}
+                    </div>
+                    <div className="listDetail_Item">
+                        {list.name}
+                    </div>
+                    <div className="listDetail_Item">
+                        {list.name}
+                    </div>
+                    <div className="listDetail_Item">
+                        {list.name}
+                    </div>
+
+                </div>
+            )}
+        </div>
+    )
+}
--- Frontend/src/Components/List/One/List_One.jsx
+++ Frontend/src/Components/List/One/List_One.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
+import useList_One from "./useList_One";
+import List_Detail from "../Detail/List_Detail";
 
 export default function List_One({list, index}){
+
+    const {
+        listState, 
+        dispatchListState,
+        handleClick
+    } = useList_One()
 
     return(
         <Draggable draggableId={list._id} index={index}>
             {(provided) => (
-                <div ref={provided.innerRef} {...provided.draggableProps} className="listOne_Display">
-                    <div  {...provided.dragHandleProps}  className="listOne_Box">
-                        {list.name}
+                <>
+                    <div  ref={provided.innerRef} {...provided.draggableProps} className="listOne_Display">
+                        <div onClick={handleClick} {...provided.dragHandleProps}  className="listOne_Box">
+                            {list.name}
+                        </div>
+                        <List_Detail listState={listState} list={list}/>
+                        
                     </div>
-                </div>
+                </>
             )}
         </Draggable>
         
