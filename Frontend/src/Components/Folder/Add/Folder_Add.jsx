fdbf8a7ad0f9ddb8d5f7baa571df6d6c7008cfa2
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2024-05-05 14:07:41 +0200

    Nettoyage

--- Frontend/src/Components/Folder/Add/Folder_Add.jsx
+++ Frontend/src/Components/Folder/Add/Folder_Add.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { update_folderOnCreation } from "../FolderSlice";
 
 export default function Folder_Add(){
 
     const dispatch = useDispatch()
-    const folderOnCreation = useSelector(store => store.folder.folderOnCreation)
 
     return(
         <div className="addButtonFolder_Display">
